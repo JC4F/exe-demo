26156936f10db811efccd5b9e8eb0e18c3c6a99e
JC4F <lequan12122002@gmail.com>
2023-07-12 15:02:20 +0700

    add dich vu

--- js/routes.js
+++ js/routes.js
@@ -1,108 +1,112 @@
 "use strict";
 var routes = [
   {
-    path: '/',
-    Url: './index.html',
+    path: "/",
+    Url: "./index.html",
   },
 
   {
-    path: '/travel-details/',
-    url: './travel-details.html',
+    path: "/travel-details/",
+    url: "./travel-details.html",
   },
 
   {
-    path: '/all-reviews/',
-    url: './all-reviews.html',
+    path: "/all-reviews/",
+    url: "./all-reviews.html",
   },
 
   {
-    path: '/country-details/',
-    url: './country-details.html',
+    path: "/country-details/",
+    url: "./country-details.html",
   },
 
   {
-    path: '/explore-category-place/',
-    url: './explore-category-place.html',
+    path: "/explore-category-place/",
+    url: "./explore-category-place.html",
   },
 
   {
-    path: '/explore-category-tour/',
-    url: './explore-category-tour.html',
+    path: "/explore-category-tour/",
+    url: "./explore-category-tour.html",
   },
 
   {
-    path: '/gallery/',
-    url: './gallery.html',
+    path: "/gallery/",
+    url: "./gallery.html",
   },
 
   {
-    path: '/notification/',
-    url: './notification.html',
+    path: "/notification/",
+    url: "./notification.html",
   },
 
   {
-    path: '/language/',
-    url: './language.html',
+    path: "/language/",
+    url: "./language.html",
   },
 
   {
-    path: '/settings/',
-    url: './settings.html',
+    path: "/settings/",
+    url: "./settings.html",
   },
 
   {
-    path: '/profile-settings/',
-    url: './profile-settings.html',
+    path: "/profile-settings/",
+    url: "./profile-settings.html",
   },
 
   {
-    path: '/account-settings/',
-    url: './account-settings.html',
+    path: "/account-settings/",
+    url: "./account-settings.html",
   },
 
   {
-    path: '/password/',
-    url: './password.html',
-  },
-
-   {
-    path: '/allow-notification-settings/',
-    url: './allow-notification-settings.html',
+    path: "/password/",
+    url: "./password.html",
   },
 
   {
-    path: '/privacy/',
-    url: './privacy.html',
+    path: "/allow-notification-settings/",
+    url: "./allow-notification-settings.html",
   },
 
   {
-    path: '/help/',
-    url: './help.html',
+    path: "/privacy/",
+    url: "./privacy.html",
   },
 
   {
-    path: '/help-details/',
-    url: './help-details.html',
+    path: "/help/",
+    url: "./help.html",
   },
 
   {
-    path: '/about-us/',
-    url: './about-us.html',
+    path: "/help-details/",
+    url: "./help-details.html",
   },
 
   {
-    path: '/sign-in/',
-    url: './sign-in.html',
+    path: "/about-us/",
+    url: "./about-us.html",
   },
 
   {
-    path: '/sign-up/',
-    url: './sign-up.html',
+    path: "/sign-in/",
+    url: "./sign-in.html",
   },
 
   {
-    path: '/forgot-password/',
-    url: './forgot-password.html',
+    path: "/sign-up/",
+    url: "./sign-up.html",
   },
 
+  {
+    path: "/forgot-password/",
+    url: "./forgot-password.html",
+  },
+
+  {
+    path: "/list-service/",
+    url: "./list-service.html",
+  },
 ];
